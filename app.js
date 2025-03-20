42a553601d967518317f08d7544f59273d630966
Sanchit Agarwal <sanchita.g893@gmail.com>
2025-03-21 00:28:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "https://mysecrets-mdbn.onrender.com/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   // console.log(profile);
